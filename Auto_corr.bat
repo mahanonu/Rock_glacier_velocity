8bb6fca5ca89df7c1f600d031797eb4159bca957
mahanonu <85451599+mahanonu@users.noreply.github.com>
2022-01-05 21:27:25 +0100

    Update Auto_corr.bat

--- Auto_corr.bat
+++ Auto_corr.bat
@@ -1,34 +1,34 @@
 @echo off
-# Fait la différence entre deux shapefiles: l'inventaire des glaciers rocheux et une couche d'occupation des sols pertinents
+#Fait la différence entre deux shapefiles: l'inventaire des glaciers rocheux et une couche d'occupation des sols pertinents
 saga_cmd shapes_polygons 15 -A Inventory.shp -B Ground_occupation.shp -RESULT diff.shp
 
-# Rentre dans l'arborescence de dossier des images à traiter
+#Rentre dans l'arborescence de dossier des images à traiter
 for /d %%i in ("D*") DO (
     for  %%k in ("%%i\*.asc") DO (
-    # Calcule la pente de chaque tuile contenant un glacier à partir d'un MNT
+    #Calcule la pente de chaque tuile contenant un glacier à partir d'un MNT
     saga_cmd ta_morphometry 0 -ELEVATION %%k -SLOPE %%i\slope.sgrd -UNIT_SLOPE 1
-    # Change la définition de la pente pour la détériorer (temps de calculs trop longs et plantages)
+    #Change la définition de la pente pour la détériorer (temps de calculs trop longs et plantages)
     saga_cmd grid_tools 0 -INPUT %%i\slope.sgrd -OUTPUT %%i\slope.sgrd -TARGET_USER_SIZE 20
-    # Transforme la grille de pente en shapefile
+    #Transforme la grille de pente en shapefile
     saga_cmd shapes_grid 6 -GRID %%i\slope.sgrd -POLYGONS %%i\slope.shp
-    # Stocke l'execution du tri des pentes superieures à 40 degrés dans un txt
+    #Stocke l'execution du tri des pentes superieures à 40 degrés dans un txt
     echo shapes_tools 3 -SHAPES %%i\slope.shp -FIELD 1 -EXPRESSION="a > 40" > %%i\selec_pente.txt | echo shapes_tools 6 -INPUT %%i\slope.shp -OUTPUT %%i\slope_sup_seuil.shp >> %%i\selec_pente.txt
-    # Execute le txt
+    #Execute le txt
     saga_cmd %%i\selec_pente.txt
-    # Fusionne les différents polygones en 1 seul (améliore le temps de calcul)
+    #Fusionne les différents polygones en 1 seul (améliore le temps de calcul)
     saga_cmd shapes_polygons 5 -POLYGONS slope_sup_seuil.shp -DISSOLVED slope_sup_seuil.shp
-    # Fait la différence entre la couche des glaciers calculée au début et les pentes supérieures à 40 degrés
+    #Fait la différence entre la couche des glaciers calculée au début et les pentes supérieures à 40 degrés
     saga_cmd shapes_polygons 15 -A diff.shp -B %%i\slope_sup_seuil.shp -RESULT diff.shp
     #supprime les pentes calculées
     del /s %%i\*slope*
     #Parcours les orthoimages aux différentes dates
     for /d %%j in ("%%i\*") DO (
         for %%l in ("%%j\*.tif") DO (
-            # Lit les trois bandes RGB de l'image
+            #Lit les trois bandes RGB de l'image
             saga_cmd io_gdal 0 -FILES %%l -GRIDS "%%j\rouge.sgrd;%%j\vert.sgrd;%%j\bleu.sgrd" -MULTIPLE 0
-            # Fait une composition RGB de l'image pour être utilisable dans les algorithmes suivants
+            #Fait une composition RGB de l'image pour être utilisable dans les algorithmes suivants
             saga_cmd grid_visualisation 3 -R_GRID %%j\rouge.sgrd -G_GRID %%j\vert.sgrd -B_GRID %%j\bleu.sgrd -A_GRID NULL -RGB %%j\rgb.sgrd -METHOD 4 -STDDEV 2.000000
-            # Garde la partie intéressante des images en faisant la différence avec le shapefile des zones d'intérets des glaciers
+            #Garde la partie intéressante des images en faisant la différence avec le shapefile des zones d'intérets des glaciers
             saga_cmd grid_tools 31 -GRIDS %%j\rgb.sgrd -CLIPPED %%j\rgb.sgrd -EXTENT 3 -POLYGONS diff.shp
             #Supprime les bandes RGB
             del /s %%j\*rouge*
